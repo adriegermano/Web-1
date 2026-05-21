9ad7f30de3fd08b5f1bc888a938c22926fe1f73b
Adriel Germano <adrielgermano@unipam.edu.br>
2026-05-21 00:03:05 +0000

    Aula 20/05 - JS

---
+++ JavaScript/aula_ex02.js
@@ -0,0 +1,17 @@
+/*Parênteses serve para receber dados e informa que é uma função*/
+
+/*function saudacao(nome){
+    return "Olá, " + nome + "!";
+}
+
+let frase = saudacao("Adriel");
+
+console.log(frase);*/
+
+function soma(a,b){
+    return a + b;
+}
+
+let resultado = soma(17, 23);
+
+console.log(resultado);
