090135399b50bd5d03d4fdcc597344ad7185db49
Adriel Germano <adrielgermano@unipam.edu.br>
2026-05-21 00:23:12 +0000

    Final da aula do dia 20/05 - JavaScript

--- JavaScript/aula_ex02.js
+++ JavaScript/aula_ex02.js
@@ -8,10 +8,35 @@ let frase = saudacao("Adriel");
 
 console.log(frase);*/
 
+/*SOMA 
 function soma(a,b){
     return a + b;
 }
 
 let resultado = soma(17, 23);
 
-console.log(resultado);
+console.log("O resultado da soma é: " + resultado);*/
+
+
+/*MULTIPLICAÇÃO
+function multiplicacao(a,b){
+    return a * b;
+}
+
+let resultado = multiplicacao(5, 4);
+
+console.log("O resultado da multiplicação é de: " + resultado);
+*/
+
+/*OUTRO JEITO DA MULTIPLICAÇÃO
+
+console.log(multiplicacao(7,8));
+*/
+
+/*OUTRO JEITO DA MULTIPLICAÇÃO COM INTERPOLAÇÃO*/
+function multiplicacao(a, b){
+    console.log(`O valor da multiplicação entre ${a} e ${b} é de: ${(a * b)}`);
+}
+multiplicacao(5,5);
+
+
