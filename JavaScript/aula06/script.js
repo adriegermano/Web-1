71c6263aae9dffd064b8304538b0c79fa0923c9d
Adriel Germano <adrielgermano@unipam.edu.br>
2026-06-04 00:26:59 +0000

    Aula do dia 03/06

---
+++ JavaScript/aula06/script.js
@@ -0,0 +1,26 @@
+/*
+function soma() {
+            result.value = parseInt(v1.value) + parseInt(v2.value);
+
+            parseInt() - Transforma o tipo do elemento
+
+        }
+        
+        function multiplicacao() {
+            result.value = parseInt(v1.value) * parseInt(v2.value);
+        }
+        function divisao() {
+            result.value = parseFloat(v1.value) / parseFloat(v2.value);
+        }
+        function subtracao(){
+            result.value = parseInt(v1.value) - parseInt(v2.value);
+        }
+    */
+
+
+function calc(){
+    soma.value = parseInt(v1.value) + parseInt(v2.value);
+    sub.value = parseInt(v1.value) - parseInt(v2.value);
+    mult.value = parseInt(v1.value) * parseInt(v2.value);
+    divi.value = parseFloat(v1.value) / parseFloat(v2.value);
+}
---
+++ JavaScript/aula07/script.js
@@ -0,0 +1,29 @@
+/*
+function calc(){
+    if(op.value == "+"){
+        result.value = parseInt(v1.value) + parseInt(v2.value);
+    }
+    else if(op.value == "-"){
+        result.value = parseInt(v1.value) + parseInt(v2.value);
+    }
+    else if(op.value == "*"){
+        result.value = parseInt(v1.value) * parseInt(v2.value);
+    }
+    else if(op.value == "/"){
+        result.value = parseInt(v1.value) / parseInt(v2.value);
+    }
+}
+*/
+
+/*OU*/
+
+function calc(){
+    if(op.value == "+")
+        result.value = parseInt(v1.value) + parseInt(v2.value);
+    else if(op.value == "-")
+        result.value = parseInt(v1.value) - parseInt(v2.value);
+    else if(op.value == "*")
+        result.value = parseInt(v1.value) * parseInt(v2.value);
+    else if(op.value == "/")
+        result.value = parseInt(v1.value) / parseInt(v2.value);
+}
